398a507dc2bc8c9dd6ef09fc3424e525e617fbf1
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-04-14 14:09:41 -0500

    iterative_sorting.py: bubble_sort completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -9,22 +9,31 @@ def selection_sort(arr):
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
-
         # TO-DO: swap
-
         if smallest_index:
             arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
-
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-
+    # Make a flag to show if swaps have occurred
+    swaps_occurred = True
+    # Run until you get through a loop without any swaps
+    while swaps_occurred:
+        swaps_occurred = False
+        # For each element in the array...
+        for i in range(len(arr) - 1):
+            # Check it's neighbor to the right...
+            if arr[i] > arr[i+1]:
+                # If neighbor is smaller, swap and make Flag true
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swaps_occurred = True
     return arr
 
 
 # STRETCH: implement the Count Sort function below
+
 def count_sort(arr, maximum=-1):
 
     return arr
